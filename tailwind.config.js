fa46dc54f009d5d80faeeb2726ad4e323bbf583f
Mahboob Iqbal <mahboobiqbal774@gmail.com>
2025-08-09 23:20:02 +0500

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      mobile: "480px",
+      // => @media (min-width: 480px) { ... }
+
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        normal: "400",
+      },
+      fontSize: {
+        base: "16px",
+      },
+      lineHeight: {
+        base: "28px",
+      },
+    },
+  },
+  plugins: [],
+};
