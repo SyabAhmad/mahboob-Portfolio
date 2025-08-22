9af09e94a0baca5fef92f53b188fd44430ca0be4
syedsyab <syabblogger@gmail.com>
2025-08-22 23:24:43 +0500

    Update base path in Vite configuration to root for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/",
   server: {
     port: 3000,
     host: '127.0.0.1', // Use IPv4 loopback
